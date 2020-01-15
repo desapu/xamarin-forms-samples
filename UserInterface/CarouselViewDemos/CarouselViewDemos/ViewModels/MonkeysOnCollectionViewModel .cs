2004e5a9c518555d402de9e52bb34328f122ae33
Carlos Forero <ecarlosforero@gmail.com>
2020-01-15 00:30:03 -0500

    Recycling problem of CarouselView Position in CollectionView

---
+++ UserInterface/CarouselViewDemos/CarouselViewDemos/ViewModels/MonkeysOnCollectionViewModel .cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace CarouselViewDemos.ViewModels
+{
+    public class MonkeysOnCollectionViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<MonkeysRowViewModel> Rows { get; private set; }
+
+        public MonkeysOnCollectionViewModel()
+        {
+            Rows = new ObservableCollection<MonkeysRowViewModel>();
+            BuildMonkeys();
+        }
+
+        private void BuildMonkeys()
+        {
+            for (var i = 0; i < 16; i += 2) {
+                Rows.Add(new MonkeysRowViewModel(i, 2));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
---
+++ UserInterface/CarouselViewDemos/CarouselViewDemos/Views/CollectionView/CollectionViewPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CarouselViewDemos.Views
+{
+    public partial class CollectionViewPage : ContentPage
+    {
+        public CollectionViewPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
