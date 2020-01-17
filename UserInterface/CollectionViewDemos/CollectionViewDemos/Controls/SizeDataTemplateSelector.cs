00c5fa5220425e499014a39fc6d17f335831ace7
Carlos Forero <ecarlosforero@gmail.com>
2020-01-17 01:29:55 -0500

    Recycling problems with multi-size DataTemplate

---
+++ UserInterface/CollectionViewDemos/CollectionViewDemos/Controls/SizeDataTemplateSelector.cs
@@ -0,0 +1,27 @@
+﻿using CollectionViewDemos.Models;
+using Xamarin.Forms;
+
+namespace CollectionViewDemos.Controls
+{
+    public class SizeDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate SizeSmall { get; set; }
+        public DataTemplate SizeMedium { get; set; }
+        public DataTemplate SizeLarge { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            DataTemplate template = SizeSmall;
+            var location = ((Animal)item).Location;
+
+            if (location.Contains("Asia")) {
+                template = SizeLarge;
+            } else if (location.Contains("America"))
+            {
+                template = SizeMedium;
+            }
+
+            return template;
+        }
+    }
+}
---
+++ UserInterface/CollectionViewDemos/CollectionViewDemos/Views/Layout/MultipleDataTemplateSizePage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using CollectionViewDemos.ViewModels;
+using Xamarin.Forms;
+
+namespace CollectionViewDemos.Views
+{
+    public partial class MultipleDataTemplateSizePage : ContentPage
+    {
+        public MultipleDataTemplateSizePage()
+        {
+            InitializeComponent();
+            BindingContext = new AnimalsViewModel();
+        }
+    }
+}
